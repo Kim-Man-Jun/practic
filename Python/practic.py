8275c416f3e2a9cd0eb36eb4ba61bde1839e24c0
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2022-12-16 12:31:07 +0900

    Update practic.py

--- Python/practic.py
+++ Python/practic.py
@@ -1,1 +1,19 @@
-print("Hello Python")
+print("Hello Python")   #파이썬의 주석표시
+
+print("Hello", "Bye")   #여러 단어를 한줄에 출력하고 싶을때는 ","를 넣자
+
+print()    #빈 줄을 출력
+
+#혼자 공부하는 파이썬 p39
+print("# 하나만 출력합시다.")
+print("Hello Python Programming...!")
+print()
+
+print("# 여러개를 출력해보자")
+print(10, 20, 30, 40, 50)
+print()
+
+print("# 아무것도 출력하지 않음.")
+print()
+print("--확인선--")
+
