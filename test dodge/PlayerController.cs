511b35e1333d511b1c4b91c357777dbbb7e9c78f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2022-12-29 12:23:02 +0900

    Update PlayerController.cs

--- test dodge/PlayerController.cs
+++ test dodge/PlayerController.cs
@@ -9,11 +9,20 @@ public class PlayerController : MonoBehaviour
   
   void Start()
   {
-  
+    playerRigidbody = GetComponent<Rigidbody>();  //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
   }
   
   void Update()
   {
+    float xInput = Input.GetAxis("Horizontal"); //수평축 입력값을 xInput에 저장
+    float xSpeed = xInput * speed;  //수평축 속도를 구함
     
+    Vector3 newVelocity = new Vector3(xSpeed, 0f, 0f);  //Vector3 속도를 뒤의 값으로 생성
+    playerRigidbody.velocity = newVelocity; //playerRigidbody.velocity 속도는 newVelocity의 값으로
+  }
+  
+  public void Die()
+  {
+  
   }
 }
