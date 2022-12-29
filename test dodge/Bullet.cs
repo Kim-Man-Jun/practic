f9619f6b51e93d994e0062c91af849a64d48394d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2022-12-29 16:15:31 +0900

    Update Bullet.cs

--- test dodge/Bullet.cs
+++ test dodge/Bullet.cs
@@ -14,4 +14,17 @@ public class Bullet : MonoBehaviour
     
     Destroy(this.gameObject, 5.0f);
   }
+  
+  void OnTriggerEnter(Collider other)
+  {
+    if(other.tag == "Player")
+    {
+      PlayerController playerController = other.GetComponent<PlayerController>();
+      
+      if(playerController != null)  //PlayerController이 컴포넌트를 가져오는데 성공했으면
+      {
+        playerController.Die(); //PlayerController 컴포넌트의 die메서드 실행
+      }      
+    }
+  }
 }
