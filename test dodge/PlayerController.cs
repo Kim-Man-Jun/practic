07b839ea57926cb5eb55f75c56b0d4bff6721e65
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2022-12-29 12:18:33 +0900

    Create PlayerController.cs

---
+++ test dodge/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  private Rigidbody playerRigidbody;
+  public float speed = 8.0f;
+  
+  void Start()
+  {
+  
+  }
+  
+  void Update()
+  {
+    
+  }
+}
