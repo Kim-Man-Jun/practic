6c3d1e29b6c4506f21ad03367a142b26d77247e1
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2022-12-29 12:31:45 +0900

    Create Bullet.cs

---
+++ test dodge/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  public float speed = 8.0f;
+  private Rigidbody bulletRigidbody;
+  
+  void Statr()
+  {
+  }
+}
