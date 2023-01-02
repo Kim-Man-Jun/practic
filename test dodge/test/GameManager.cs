5df153ce1266d5eaa8a2f75296702bdad80f51ea
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-01-02 16:27:08 +0900

    Update GameManager.cs

--- test dodge/test/GameManager.cs
+++ test dodge/test/GameManager.cs
@@ -6,11 +6,24 @@ using UnityEngine.SceneManagement;  //씬 관리 관련 라이브러리
 
 public class GameManager : MonoBehaviour
 {
+  public GameObject gameoverText;
+  public Text timeText;
+  public Text recordText;
+  
+  private float surviveTime;
+  private bool isGameover;
+  
   void Start()
   {
+    surviveTime = 0;
+    isGameover = false;
   }
   
   void Update()
+  {
+  }
+  
+  public void EndGame()
   {
   }
 }
