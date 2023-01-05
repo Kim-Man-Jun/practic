2196fec68f5391b690aece21cf39471f1687de05
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-01-05 12:23:19 +0900

    Update GunData.cs

--- ZombieSurvive/practice/GunData.cs
+++ ZombieSurvive/practice/GunData.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class GunData : MonoBehaviour
 {
-  public AudioClip shotClip;
-  public AudioClip reloadClip;
+  public AudioClip shotClip;                //발사 소리
+  public AudioClip reloadClip;              //재장전 소리
   
-  public float damage = 25;
+  public float damage = 25;                 //총 공격력
   
-  public int startAmmoRemain = 100;
-  public int magCapacity = 25;
+  public int startAmmoRemain = 100;         //첫 시작시 주어지는 탄알수
+  public int magCapacity = 25;              //탄창 용량
   
-  public float timeBetFire = 0.12f;
-  public float reloadTime = 1.8f;
+  public float timeBetFire = 0.12f;         //탄알 발사 간격
+  public float reloadTime = 1.8f;           //재장전에 걸리는 시간
 }
