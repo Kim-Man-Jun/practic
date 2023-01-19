0d574e4ad28c2b6442fda6c5fca4864e2df99b4f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-01-19 12:15:35 +0900

    Update PlayerController.cs

--- 2D Platfomer/PlayerController.cs
+++ 2D Platfomer/PlayerController.cs
@@ -4,12 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+  public float jumpForce = 500f;
+  
+  private int jumpCount = 0;
+  private bool isGround = false;
+  private bool isDead = false;
+  
+  private Rigidbody2D playerRigidbody;
+  
   void Start()
   {
+    playerRigidbody = GetComponent<Rigidbody2D>();
   }
   
   void Update()
   {
+    if(isDead)
+    {
+      return;
+    }
+    
+    if(Input.GetKeyDown(space) && jumpCount < 2)
+    {
+      jumpCount++;
+      playerRigidbody.velocity = Vector2.zero;
+      playerRigidbody.AddFocrce(new Vector2(0, jumpForce));
+    }
+    else if(Input.GetKeyDown(space) && playerRigidbody.Vector2.y > 0)
+    {
+      playerRigidbody.velocity = playerRigidbody.velocity * 0.8f;
+    }
   }
   
 }
