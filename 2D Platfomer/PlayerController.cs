60b68ce411f57e0a1c9c3d4cbd6c67f0ca4179f1
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-01-29 13:52:26 +0900

    Update PlayerController.cs

--- 2D Platfomer/PlayerController.cs
+++ 2D Platfomer/PlayerController.cs
@@ -3,85 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{  
-  private Rigidbody2D playerRigidbody;
-  private float axisH = 0.0f;
-  public float speed = 4.0f;
-  
-  public float jump = 9.0f;
-  public LayerMask groundLayer;
-  private bool onGround = false;  
-  private bool goJump = false;
-  
-  public static string gameState = "Playing";
-  
-  void Start()
-  {
-    playerRigidbody = GetComponent<Rigidbody2D>();
-    gameState = "Playing";
-  }
-  
-  void Update()
-  {
-    if(gameState != "Playing")
+{
+    Rigidbody2D rbody;          // Rigidbody2D 형의 변수
+    float axisH = 0.0f;
+    public float speed = 3.0f;  // 이동속도
+
+    public float jump = 9.0f;       // 점프력
+    public LayerMask groundLayer;   // 착지할 수 있는 레이어
+    bool goJump = false;            // 점프 개시 플래그
+    bool onGround = false;          // 지면에 서있는 플래그
+
+    Animator animator;
+    public string idleAnime = "PlayerIdle";
+    public string jumpAnime = "PlayerJump";
+    public string runAnime = "PlayerRun";
+    string nowAnime = "";
+    string oldAnime = "";
+
+
+    void Start()
     {
-      return;
+        // Rigidbody2D 가져오기
+        rbody = this.GetComponent<Rigidbody2D>();
+        // Animator 가져오기
+        animator = GetComponent<Animator>();
+        nowAnime = idleAnime;
+        oldAnime = idleAnime;
     }
-    axisH = Input.GetAxisRaw("Horizontal");
-    if(axisH > 0.0f)
+
+    void Update()
     {
-      transform.localScale = new Vector2(1,1);
+        axisH = Input.GetAxisRaw("Horizontal");
+
+        if (axisH > 0.0f)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (axisH < 0.0f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump(); // 점프
+        }
     }
-    else if(axisH < 0.0f)
+
+    void FixedUpdate()
     {
-      transform.localScale = new Vector2(-1,1);
+        onGround = Physics2D.Linecast(transform.position,
+                                      transform.position - (transform.up * 0.1f),
+                                      groundLayer);
+
+        if (onGround || axisH != 0)  //지면 위 또는 속도가 0이 아닐때
+        {
+            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        }
+
+        if (onGround && goJump)
+        {
+            Vector2 jumpPw = new Vector2(0, jump);
+            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+            goJump = false;
+        }
+
+        if (onGround)
+        {
+            if (axisH == 0)
+            {
+                nowAnime = idleAnime;
+            }
+            else
+            {
+                nowAnime = runAnime;
+            }
+        }
+        else
+        {
+            nowAnime = jumpAnime;
+        }
+
+        if (nowAnime != oldAnime)
+        {
+            oldAnime = nowAnime;
+            animator.Play(nowAnime);
+        }
     }
-    
-    if(Input.GetButtonDwon("Jump"))
+
+    public void Jump()
     {
-      Jump();
+        goJump = true;
+        Debug.Log("점프 버튼 누름");
     }
-  }
-  
-  void FixedUpdate()
-  {
-    if(gameState != "Playing")
-    {
-      return;
-    }
-    
-    onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundLayer);
-    
-    if(onGround || axisH != 0)  //지면 위 또는 속도가 0이 아닐때
-    {
-      playerRigidbody.velocity = new Vector2(axisH * speed, playerRigidbody.velocity.y);
-    }
-    if(onGround && goJump)
-    {
-      Vector2 jumpPw = new Vector2(0, jump);
-      playerRigidbody.AddForce(jumpPw, ForceMode2D.Impulse);
-      goJump = false;
-    }
-  }
-  
-  public void Jump();
-  {
-    goJump = true;
-  }
-  
-  public void GameOVer()
-  {
-    gameState = "gameover";
-    Gamestop();
-    GetComponent<CapsuleCollider2D>().enabled = false;
-    playerRigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
-  }
-  
-  void GameStop()
-  {
-    Rigidbody2D playerRigidbody = GetComponent<Rigidbody2D>();
-    playerRigidbody.velocity = new Vector2(0, 0);
-  }
 
 
 }
