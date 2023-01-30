02710154476855a63c95506c8e700bea45d76854
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-01-30 12:30:47 +0900

    Create MovingBlock.cs

---
+++ 2D Platfomer i wanna/MovingBlock.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour
+{
+  public float moveX = 0.0f;
+  public float moveY = 0.0f;
+  public float times = 0.0f;
+  public float weight = 0.0f;
+  public bool isMoveWhenOn = false;
+  
+  public bool isCanMove = true;
+  float perDx;
+  float perDy;
+  Vector3 defPos;
+  bool isReverse = false;
+  
+  void Start()
+  {
+  }
+  
+  void Update()
+  {
+  }
+}
