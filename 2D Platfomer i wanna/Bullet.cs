14dc2e40912b80c5f2d0e042b3a083acee6f575e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-02 17:02:39 +0900

    Create Bullet.cs

---
+++ 2D Platfomer i wanna/Bullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+  void Update()
+  {
+    transform.Translate(Vector2.forward * 10.0f);
+    //프레임마다 오브젝트를 정면을 향해 발사
+  }
+}
