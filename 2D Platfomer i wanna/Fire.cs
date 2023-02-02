86b19b8d3d958c20ea1117722f46ff8e4ca5f11a
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-02 22:22:39 +0900

    Update Fire.cs

--- 2D Platfomer i wanna/Fire.cs
+++ 2D Platfomer i wanna/Fire.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-  public GameObject Bullet;
-  public Transform FirePos;
-  
-  void Update()
-  {
-    if(Input.GetButtonDown("fire1"))
+    public GameObject BulletPrefab;
+    public Transform FirePos;
+    public float cooltime;
+    private float curtime;
+
+    void Update()
     {
-      Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
-      //bullet를 FirePos.transform.position 위치에 FirePos.transform.rotation 회전값으로 복제한다
+        if (curtime <= 0)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                GameObject Bullet = Instantiate(BulletPrefab, FirePos.transform.position, FirePos.transform.rotation);
+                //bullet를 FirePos.transform.position 위치에 FirePos.transform.rotation 회전값으로 복제한다
+            }
+            curtime = cooltime;
+        }
+        curtime -= Time.deltaTime;
     }
-  }
 }
