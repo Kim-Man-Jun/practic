0783a385e5db3285defa7156eda6f785cb0b0a69
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-02 22:23:07 +0900

    Update Bullet.cs

--- 2D Platfomer i wanna/Bullet.cs
+++ 2D Platfomer i wanna/Bullet.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20.0f; //총알 속도
-    private Rigidbody2D bulletRigidbody; //이동에 사용할 rigidbody 컴포넌트
 
     void Start()
     {
-        bulletRigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     private void Update()
     {
-        bulletRigidbody.velocity = transform.right * speed;
+        if (transform.rotation.y == 0)
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        else if (transform.rotation.y == 180)
+        {
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+        }
     }
 
     private void OnBecameInvisible()
