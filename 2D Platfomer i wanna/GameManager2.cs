729b769efef0eab39fea5a432bd123ddcb65bd25
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-07 12:14:32 +0900

    Update GameManager2.cs

--- 2D Platfomer i wanna/GameManager2.cs
+++ 2D Platfomer i wanna/GameManager2.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager2 : MonoBehaviour
 {
+  public static GameManager instance;
+  
+  public bool isGameover = false;
+  public GameObject gameoverUI;
+  
+  void Awake()
+  {
+    if(instance == null)
+    {
+      instance = this;
+    }
+    else
+    {
+      Destroy(gameObject);
+    }
+  }
 
   void Start()
   {
