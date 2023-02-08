9db78d647863c0b2746a5da2557225e17300c484
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-08 12:05:09 +0900

    Update and rename GameManager2.cs to GameManager.cs

--- 2D Platfomer i wanna/GameManager.cs
+++ 2D Platfomer i wanna/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager2 : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
   public static GameManager instance;
   
