70f8e228d43d37d4a2cf84ed4c819e4da6d0b30a
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-09 12:31:36 +0900

    Update DataManager.cs

--- 2D Platfomer i wanna/DataManager.cs
+++ 2D Platfomer i wanna/DataManager.cs
@@ -22,7 +22,25 @@ public class DataManager : MonoBehaviour
   }
   
   string GameDataFileName = "GameData.json";      //게임 데이터 파일 이름 설정, "원하는 이름(영문으로).json"
+  
   public Data date = new Data();                  //저장용 클래스 변수
   
+  public void LoadGameData()                      //불러오기
+  {
+    string filePath = Application.persistentDataPath + "/" + GameDataFileName;
+    if(File.Exists(filePath))                     //저장된 게임이 있을 경우
+    {
+      string FromJsonData = File.ReadAllText(filePath);
+      data = JsonUtility.FromJson<Data>(FromJsonData);
+      print("불러오기 완료");
+    }
+  }
   
+  public void SaveGameData()
+  {
+    string ToJsonData = JsonUtility.ToJson(data, true);
+    string filePath = Application.persistentDataPath + "/" + GameDataFileName;
+    File.WriteAllText(filePathm ToJsonData);
+    print("저장 완료");
+  }
 }
