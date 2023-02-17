4d2bea997aa1438f988a504aa88d77d454f9a32b
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-17 16:24:59 +0900

    Update GameManager.cs

--- 2D Platfomer i wanna/GameManager/GameManager.cs
+++ 2D Platfomer i wanna/GameManager/GameManager.cs
@@ -37,6 +37,11 @@ public class GameManager : MonoBehaviour
   {
     StartingPos = GameObject.FindGameObjectWithTag("Start").transform.position;
     StartingRotate = GameObject.FindGameObjectWithTag("Start").transform.rotation;
+    
+    if(stageLevel > 0)
+    {
+      StartGame();  //이부분 체크해야함
+    }
   }
   
   void Update()         //게임오버 상태에서 재시작 가능하게 만들기
@@ -56,6 +61,9 @@ public class GameManager : MonoBehaviour
   public static void clearGame()
   {
     isClear = true;
+    
+    stageLevel++;
+    SceneManager.LoadScene(stageLevel, LoadSceneMode.Single);
   }
   
 }
