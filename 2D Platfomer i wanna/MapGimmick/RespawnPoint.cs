df9d02394fa84a2fccd428a9feaa1cbdd589eb62
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-21 14:12:45 +0900

    Create RespawnPoint.cs

---
+++ 2D Platfomer i wanna/MapGimmick/RespawnPoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+  void Start()
+  {
+  }
+  
+  void Update()
+  {
+  }
+  
+}
