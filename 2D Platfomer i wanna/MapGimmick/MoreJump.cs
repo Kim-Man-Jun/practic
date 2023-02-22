11e5bcaa5dc88b8a37dec60158c55dafbea67c28
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-22 12:01:02 +0900

    Update MoreJump.cs

--- 2D Platfomer i wanna/MapGimmick/MoreJump.cs
+++ 2D Platfomer i wanna/MapGimmick/MoreJump.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoreJump : MonoBehaviour
 {
-  public int jumpCount;
+  GameObject Player;
   
   void Start()
   {
@@ -14,6 +14,22 @@ public class MoreJump : MonoBehaviour
   {
   }
   
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if(collision.gameObject.tag == "Player"
+    {
+      if(Player.jumpCount >= 1)
+      {
+        Player.jumpCount--;
+        Destroy(this.gameObject);
+      }
+      
+      else if(Player.jumpCount ==0)
+      {
+        Player.Destroy(this.gameObject);
+      }
+    }
+  }
   //circle collider 추가 - is trigger 체크
   //prefab 처리
 }
