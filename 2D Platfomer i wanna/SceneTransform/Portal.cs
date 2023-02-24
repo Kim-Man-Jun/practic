c5eeecc70c29c3d2cdae414e0c5a7a4587d7cb6e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-24 12:29:59 +0900

    Update Portal.cs

--- 2D Platfomer i wanna/SceneTransform/Portal.cs
+++ 2D Platfomer i wanna/SceneTransform/Portal.cs
@@ -26,4 +26,5 @@ public class Portal : MonoBehaviour
   }
   
   //사이트 주소 https://medium.com/@qldrhqorhsh/unity-tutorial-6-scene-%EB%A7%B5-%EC%9D%B4%EB%8F%99-e92d05840c2d 
+  //스테이지 하나를 크게 만든 다음에 씬 전환하는 것처럼 카메라 이동 제한을 두고 진행을 해보는 식으로
 }
