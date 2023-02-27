08e2e94ef909acb3056e9072508848dcac52621b
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-27 12:25:13 +0900

    Update Enemy.cs

--- 2D Platfomer i wanna/Enemy/Enemy.cs
+++ 2D Platfomer i wanna/Enemy/Enemy.cs
@@ -22,6 +22,20 @@ public class Enemy : MonoBehaviour
   
   void Move()
   {
+    Vetor3 moveVelocity = Vector3.zero;
     
+    if(movementSet == 1)
+    {
+      moveVelocity = Vector3.left;
+      transform.localScale = new Vectro3(1, 1, 1);
+    }
+    
+    if(movementSet == 2)
+    {
+      moveVelocity = Vector3.Right;
+      transform.localScale = new Vectro3(-1, 1, 1);
+    }
+    
+    transform.position += moveVelocity * moveSpeed * Time.deltaTime;
   }
 }
