71734e1345e2c8cab79e80da4dd315bf7c37c94f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-27 12:36:43 +0900

    Update BasicEnemy.cs

--- 2D Platfomer i wanna/Enemy/BasicEnemy.cs
+++ 2D Platfomer i wanna/Enemy/BasicEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class BasicEnemy : MonoBehaviour
 {
   public float moveSpeed = 1.0f;
   
