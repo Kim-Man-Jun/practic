c4b108de09ecc3bb3d58217219101cd4c2b9602e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-02-27 12:35:34 +0900

    Update Enemy.cs

--- 2D Platfomer i wanna/Enemy/Enemy.cs
+++ 2D Platfomer i wanna/Enemy/Enemy.cs
@@ -13,6 +13,8 @@ public class Enemy : MonoBehaviour
   void Start()
   {
     animator = gameObject.GetComponentInChildren<animator>();
+    
+    StartCoroutine("ChangeMovement");
   }
   
   void FixedUpdate()
@@ -38,4 +40,26 @@ public class Enemy : MonoBehaviour
     
     transform.position += moveVelocity * moveSpeed * Time.deltaTime;
   }
+  
+  IEnumerator ChangeMovement()
+  {
+    movementSet = Random.Range(0, 3);   //이럴 경우 랜덤 숫자는 0에서 3 미만까지, 즉 0, 1, 2가 나온다. 한도에서 +1 하는거 잊지말것
+    
+    if(movementSet == 0)
+    {
+      animator.SetBool("isMoving", false);
+    }
+    else
+    {
+      animator.SetBool("isMoving", true);
+    }
+    
+    yield return new WaitForSeconds(2.0f);
+    
+    StartCoroutine("ChangeMovement");
+  }
+  //코루틴 설명 : yield는 반복 함수의 탈출 시점. new WaitForSeconds를 통해 딜레이 시간을 정해줌.
+  //2초 동안 랜덤으로 설정된 movement에서 나온 값을 기반으로 행동하게 됨
+  //그 후 yield 밑에 코루틴을 재시작 함으로써  계속 반복하게 만든다.
+  
 }
