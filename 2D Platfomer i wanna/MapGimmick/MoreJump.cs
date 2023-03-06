226518f7d17ff7597c9df2006e3fb5d4f8fe1b4b
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-03-06 15:05:22 +0900

    Update MoreJump.cs

--- 2D Platfomer i wanna/MapGimmick/MoreJump.cs
+++ 2D Platfomer i wanna/MapGimmick/MoreJump.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class MoreJump : MonoBehaviour
 {
-    public GameObject morejumpPre;
-    public float Interval;
-    
-    void Start()
-    {
-        InvokeRepeating("Respawn", 0.0f, Interval);
-    }
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
@@ -28,20 +20,8 @@ public class MoreJump : MonoBehaviour
             }
         }
     }
-   
-    private void Respawn()
-    {
-      if(this.gameObject = null)
-      {
-          Instatiate(morejumpPre, Transform MoreJump);
-      }
-    }
-
     //circle collider 추가 - is trigger 체크
     //prefab 처리
     //02.22 할일 morejump 체크 후 = 해냈다! 기분 너무 좋다. playercontroller에 있는 jumpcount를 public static로 전역변수 선언
-    //이럴 경우 굳이 제일 앞에 public Playercontroller를 선언해줄 필요가 없음.
-    //처음에 ontriggerenter가 아닌 oncollisionenter를 썼더니 기획했던 것과 달랐음
-    //trigger 물리적 충돌 없음, collision 
     //invokerepeating을 이용한 재생성 처리까지 
 }
