459767ee9221ee5c975d7c6206afbdb7944a9c96
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-03-10 15:49:25 +0900

    Update GameDateManager.cs

--- 2D Platfomer i wanna/GameSave/GameDateManager.cs
+++ 2D Platfomer i wanna/GameSave/GameDateManager.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+class Data()
+{
+  public int stage;
+  public float PlayerTransformX;
+  public float PlayerTransformY;
+}
+
 public class GameDateManager : MonoBehaviour
 {
+  public gameObject player; 
+  
+  Data player = new Data(){ stage = 1, PlayerTransformX = player.transform.x, 
+                            PlayerTransformY = player.transform.y}
+                            
 
   void Start()
   {
+    
   }
   
   void Update()
