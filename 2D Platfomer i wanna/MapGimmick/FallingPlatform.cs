c20e34310e6c2ee9e76a9662695d8c043c3bf55e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-04-06 22:52:25 +0900

    Update FallingPlatform.cs

--- 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
+++ 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
@@ -10,11 +10,9 @@ public class FallingPlatform : MonoBehaviour
     {
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         rbody.bodyType = RigidbodyType2D.Static;
-        BoxCollider2D bCol = GetComponent<BoxCollider2D>();
-        bCol.isTrigger = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         BoxCollider2D bCol = GetComponent<BoxCollider2D>();
