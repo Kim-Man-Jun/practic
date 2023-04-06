834f114c1eadb9eac7a5cb73b19304f6dcae8bdf
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-04-06 12:41:42 +0900

    Update FlashBlock.cs

--- 2D Platfomer i wanna/MapGimmick/FlashBlock.cs
+++ 2D Platfomer i wanna/MapGimmick/FlashBlock.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class FlashBlock : MonoBehaviour
 {
-    public GameObject FlashBlock;
-    public bool FBOnOff;
-    public float FBStartTime;           //morejump와 다르게 시작에도 차이점을 주기 위한 변수
-    public float FBOnOffDeleyTime;
-    public float FBLeftTime;            //닿기만 해도 사라지는 morejump와 다르게 몇초 두에 자동으로 사라지기 위한 변수
+    public GameObject FlashBlock;       
+    public bool FBOnOff;                //블록 초기 On/off 정하는 용도
+    public float FBOnOffDeleyTime;      //비활성화 후 다시 나타나는데 필요한 딜레이 시간
+    public float FBLeftTime;            //닿기만 해도 사라지는 morejump와 다르게 몇초 뒤에 자동으로 사라지기 위한 변수
 
     void Start()
     {
-        if (FBOnOff == false)
+        if (FBOnOff == false)           //블록이 비활성화 되어 있을때
         {
-            InvokeRepeating("Respawn", FBStartTime, FBOnOffDeleyTime);
+            InvokeRepeating("Respawn", 0.0f, FBOnOffDeleyTime);
         }
     }
     
     void Update()
     {
-        FBLeftTime -= Time.deltaTime;
-        if(FBLeftTime <= 0)
+        if(FBOnOff == true)
         {
-            FlashBlock.SetActive(false);
-            FBOnOff = false;
+            FBLeftTime -= Time.deltaTime;
+            if(FBLeftTime <= 0)
+            {
+                FlashBlock.SetActive(false);
+                FBOnOff = false;
+            }
         }
     }
 
