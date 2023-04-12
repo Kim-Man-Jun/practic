ab1a49b4dea748e387e7fb2496a2ea0fa792999a
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-04-12 17:13:54 +0900

    Create FadeInOut.cs

---
+++ 2D Platfomer i wanna/SceneTransform/FadeInOut.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeInOut : MonoBehaviour
+{
+  public UnityEngine.UI.Image fade;
+  
+  public bool FadeInOut;
+  
+  public float FadeIn = 1.0f;
+  public float FadeInTime = 0.0f;
+  
+  public float FadeOut = 1.0f;
+  public float FadeOutTime = 0.0f;
+
+  void Start()
+  {
+  }
+  
+  void Update()
+  {
+    
+  }
+}
