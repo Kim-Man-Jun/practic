c2a4becacc6f1546148f382b3d583e7e2402bc50
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-04-19 12:23:46 +0900

    Update BulletTime.cs

--- 2D Running/BulletTime.cs
+++ 2D Running/BulletTime.cs
@@ -15,14 +15,15 @@ public class Fire : MonoBehaviour
     {
       if(Input.GetMouseButton(1))   //마우스 오른쪽 버튼을 누르고 있을때
       {
-        Time.timeScale = 0.5;
+        Time.timeScale = 0.5f;
       }
       
       if(Input.GetMouseButtonUp(1)
       {
-        Time.timeScale = 1.0;
+        Time.timeScale = 1.0f;
       }
     }
 }
 
 //참고주소 https://luv-n-interest.tistory.com/952
+//위에 누르고 있을때 화면이 살짝 어두워지는 효과도 추가해야할 듯
