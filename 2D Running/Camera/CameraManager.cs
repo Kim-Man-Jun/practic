fb4ced54836152dfc42545a2cfaa789ef6f069d5
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-04-21 17:40:58 +0900

    Create CameraManager.cs

---
+++ 2D Running/Camera/CameraManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float leftLimit = 0.0f;
+    public float rightLimit = 0.0f;
+    public float topLimit = 0.0f;
+    public float bottomLimit = 0.0f;
+    public float yaxis = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            //카메라의 좌표 갱신
+            float x = player.transform.position.x;
+            float y = player.transform.position.y;
+            float z = transform.position.z;
+
+            //가로 방향 동기화와 양 끝에 이동 제한 적용
+            if (x < leftLimit)
+            {
+                x = leftLimit;
+            }
+            else if (x > rightLimit)
+            {
+                x = rightLimit;
+            }
+
+            //세로 방향 동기화와 위아래에 이동 제한 적용
+            if (y < bottomLimit)
+            {
+                y = bottomLimit;
+            }
+            else if (y > topLimit)
+            {
+                y = topLimit;
+            }
+            
+            Vector3 v3 = new Vector3(x, y + yaixs, z);
+            transform.position = v3;
+        }
+    }
+    
+    void Adjustment()
+    {
+        
+    }
+}
