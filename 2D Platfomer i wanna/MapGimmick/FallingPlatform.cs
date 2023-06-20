908ad3fad595a05b73a88eeb7a139c8941675b69
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-06-20 13:10:44 +0900

    Update FallingPlatform.cs

--- 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
+++ 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
@@ -10,19 +10,17 @@ public class FallingPlatform : MonoBehaviour
     {
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         rbody.bodyType = RigidbodyType2D.Static;
-        BoxCollider2D bCol = GetComponent<BoxCollider2D>();
-        bCol.isTrigger = false;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnCollision2D(Collider2D other)
     {
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         BoxCollider2D bCol = GetComponent<BoxCollider2D>();
 
+        bCol.isTrigger = true;
+
         if (other.gameObject.tag == "Player")
         {
-            bCol.isTrigger = true;
-
             if (rbody.bodyType == RigidbodyType2D.Static)
             {
                 RemainTime -= Time.deltaTime;
@@ -37,7 +35,3 @@ public class FallingPlatform : MonoBehaviour
         }
     }
 }
-
-// 방향을 바꿔보자
-// 스태틱에서 다이나믹으로 바꾸는건 맞나?
-// 차라리 무게를 부여하는 식으로 변경을 해봐야하나
