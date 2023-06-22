89f388caba098c6e6f2de596f05ba5e83927875f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-06-22 16:54:15 +0900

    Update FallingPlatform.cs

--- 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
+++ 2D Platfomer i wanna/MapGimmick/FallingPlatform.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    public float RemainTime;
+    bool chartouch = false;
+    public int RemainTime;
+    float curTime;
 
     void Start()
     {
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         rbody.bodyType = RigidbodyType2D.Static;
     }
-
-    private void OnCollisionStay2D(Collision2D other)
+    private void Update()
     {
-        if (other.gameObject.tag == "Player")
+        if (chartouch == true)
         {
-            BoxCollider2D bCol = GetComponent<BoxCollider2D>();
-            Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+            curTime += Time.fixedDeltaTime;
+        }
+    }
 
+    private void OnCollisionStay2D(Collision2D Player)
+    {
+        if (chartouch == false)
+        {
+            chartouch = true;
+        }
 
-            RemainTime -= Time.deltaTime;
+        BoxCollider2D bCol = GetComponent<BoxCollider2D>();
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
 
-            if (RemainTime <= 0)
-            {
-                bCol.isTrigger = true;
-                rbody.bodyType = RigidbodyType2D.Dynamic;
-                Destroy(this.gameObject, 5.0f);
-            }
+        Debug.Log(curTime);
+
+        if (RemainTime <= curTime)
+        {
+            bCol.isTrigger = true;
+            rbody.bodyType = RigidbodyType2D.Dynamic;
+            Destroy(this.gameObject, 5.0f);
+            chartouch = false;
         }
     }
 }
