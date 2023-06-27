740873d589715067bfb140be0a811ae504e485bb
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-06-27 16:40:42 +0900

    Update Vector3.cs

--- 유니티교육/06.27/Vector3.cs
+++ 유니티교육/06.27/Vector3.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class BallMoving : MonoBehaviour
 {
-    Rigidbody Player;
+    Rigidbody Ball;
     public float speed = 100.0f;
     Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GetComponent<Rigidbody>();
+        Ball = GetComponent<Rigidbody>();
         startPos = new Vector3(0, 0, 0);
-        Player.AddForce(-speed, 0f, speed * 0.7f);
+        Ball.AddForce(-speed, 0f, speed * 0.7f);
     }
 
     // Update is called once per frame
@@ -32,7 +32,22 @@ public class PlayerController : MonoBehaviour
             Vector3 reflectVec = Vector3.Reflect(incomVec, normalVec);
             reflectVec = reflectVec.normalized;
 
-            Player.AddForce(reflectVec * speed);
+            Ball.AddForce(reflectVec * speed);
+        }
+        startPos = transform.position;
+
+        if (coll.gameObject.CompareTag("object"))
+        {
+            Destroy(coll.gameObject);
+
+            Vector3 currPos = coll.transform.position;
+
+            Vector3 incomVec = currPos - startPos;
+            Vector3 normalVec = coll.contacts[0].normal;
+            Vector3 reflectVec = Vector3.Reflect(incomVec, normalVec);
+            reflectVec = reflectVec.normalized;
+
+            Ball.AddForce(reflectVec * speed);
         }
         startPos = transform.position;
     }
