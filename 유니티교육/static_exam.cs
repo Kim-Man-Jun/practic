351a12a32b39d30c751218cc3a378202f7909a81
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-06-28 09:23:57 +0900

    Create static_exam.cs

---
+++ 유니티교육/static_exam.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._27_C_
+{
+    class Calc
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    internal class _06
+    {
+        static void disp()
+        {
+            int c;
+            Calc calc = new Calc();
+
+            c = calc.add(8, 9);
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            disp();
+        }
+    }
+}
