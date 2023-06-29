dc0ac326a2c34e6be695fef24b8e0aae8ca28e0d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-06-29 12:53:31 +0900

    Create INFO.cs

---
+++ 유니티교육/06.29/consolerpggame/INFO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRpgGame
+{
+    public class INFO
+    {
+        public string strName;
+        public int iAttack;
+        public int iHP;
+    }
+}
