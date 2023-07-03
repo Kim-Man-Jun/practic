3698c3a7d166d01176cde8b863f3c9b989806272
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-03 10:51:31 +0900

    Update Fleid.cs

--- 유니티교육/06.29/consolerpggame/Fleid.cs
+++ 유니티교육/06.29/consolerpggame/Fleid.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,8 @@ namespace TextRpgGame
     public class Field
     {
         Player m_pPlayer = null;
-        
+        Monster m_pMonster = null;
+
         public void SetPlayer(ref Player pPlayer)
         {
             m_pPlayer = pPlayer;
@@ -35,8 +38,8 @@ namespace TextRpgGame
 
                 if (iInput <= 3)
                 {
-                    //몬스터를 만들고
-                    //전투까지
+                    CreateMonster(iInput);
+                    Fight();
                 }
 
             }
@@ -50,6 +53,78 @@ namespace TextRpgGame
             Console.WriteLine("4. 전단계");
             Console.WriteLine("=====================================");
             Console.WriteLine("맵을 선택하세요");
+        }
+
+        //생성을 도와주는 함수
+        //공장처럼 찍어냄
+        //팩토리 메서드 패턴
+
+        public void Create(string _strName, int _iHp, int _iAttack, out Monster pMonster)
+        {
+            pMonster = new Monster();
+            INFO tMonster = new INFO();
+
+            tMonster.strName = _strName;
+            tMonster.iHP = _iHp;
+            tMonster.iAttack = _iAttack;
+
+            pMonster.SetMonster(tMonster);
+
+        }
+
+        public void CreateMonster(int input)
+        {
+            switch (input)
+            {
+                case 1:
+                    {
+                        Create("초보몹", 30, 3, out m_pMonster);
+                        break;
+                    }
+                case 2:
+                    {
+                        Create("중수몹", 60, 6, out m_pMonster);
+                        break;
+                    }
+                case 3:
+                    {
+                        Create("고수몹", 90, 9, out m_pMonster);
+                        break;
+                    }
+            }
+        }
+
+
+        public void Fight()
+        {
+            int iInput = 0;
+            while (true)
+            {
+                Console.Clear();
+                m_pPlayer.Render();
+                m_pMonster.Render();
+
+                Console.WriteLine("1. 공격 2. 도망 : ");
+                iInput = int.Parse(Console.ReadLine());
+
+                if (iInput == 1)
+                {
+                    m_pPlayer.SetDamage(m_pMonster.GetMonster().iAttack);       //플레이어 데미지 주기, 몬스터 공격력 넣어서 데미지
+                    m_pMonster.SetDamage(m_pPlayer.GetInfo().iAttack);          //몬스터 데미지 추가, 플레이어 공격력 넣기
+
+                    if (m_pPlayer.GetInfo().iHP <= 0)
+                    {
+                        m_pPlayer.SetHp(100);
+                        break;
+                    }
+                }
+
+                if (iInput == 2 || m_pMonster.GetMonster().iHP <= 0)
+                {
+                    m_pMonster = null;
+                    break;
+                }
+            }
         }
 
         public Field()
