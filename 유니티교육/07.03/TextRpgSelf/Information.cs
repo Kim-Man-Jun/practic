1ed1684e342037612212f74329b6b9d5ec6b09b4
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-03 10:55:49 +0900

    Create Information.cs

---
+++ 유니티교육/07.03/TextRpgSelf/Information.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._30_RPG_TEST
+{
+    public class Infomation
+    {
+        public string Job;
+        public float HP;
+        public float ATK;
+    }
+}
