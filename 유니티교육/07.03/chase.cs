02ec8ff753372390a861795d59162ac3c51c16a4
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-03 14:13:26 +0900

    Update chase.cs

--- 유니티교육/07.03/chase.cs
+++ 유니티교육/07.03/chase.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class E : MonoBehaviour
 {
-    public GameObject destination;
+    public Transform destination;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +20,12 @@ public class E : MonoBehaviour
         //transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, 1 * Time.deltaTime);
 
         //목표에서 좀 멀었을 때는 빠른 속도로 쫓다가 근처에 왔을 땐 감속해서 접근하는 메서드
-        Vector3 speed = Vector3.zero;
-        transform.position = Vector3.SmoothDamp(transform.position, destination.transform.position, ref speed, 0.1f);
+        //Vector3 speed = Vector3.zero;
+        //transform.position = Vector3.SmoothDamp(transform.position, destination.transform.position, ref speed, 0.1f);
+
+        //slerp (현재 위치, 목표 위치, 보간 간격)
+        //반구를 그리면서 추격
+        transform.position = Vector3.Lerp(transform.position, destination.transform.position, 0.01f);
+        //transform.position = Vector3.Slerp(transform.position, destination.transform.position, 0.01f);
     }
 }
