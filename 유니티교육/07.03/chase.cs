bf5718c91beee1d464e1215c79dc64cdbc148118
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-03 13:53:05 +0900

    Update chase.cs

--- 유니티교육/07.03/chase.cs
+++ 유니티교육/07.03/chase.cs
@@ -15,6 +15,12 @@ public class E : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, 1 * Time.deltaTime);
+        //현재 위치, 목표 위치, 속력
+        //목표 위치를 잘 따라감
+        //transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, 1 * Time.deltaTime);
+
+        //목표에서 좀 멀었을 때는 빠른 속도로 쫓다가 근처에 왔을 땐 감속해서 접근하는 메서드
+        Vector3 speed = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, destination.transform.position, ref speed, 0.1f);
     }
 }
