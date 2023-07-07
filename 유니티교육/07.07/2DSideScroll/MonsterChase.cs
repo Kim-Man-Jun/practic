c45c5b47d2502a94fb3a42c18611aafb74e4174f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-07 13:30:46 +0900

    Create MonsterChase.cs

---
+++ 유니티교육/07.07/2DSideScroll/MonsterChase.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 dir = player.transform.position - transform.position;       //목표 오브젝트 방향 찾기
+        dir = dir.normalized;       //단위벡터로 만듦. 1의 크기.  방향만 나온 상태
+        float vx = dir.x * speed;
+        float vy = dir.y * speed;
+
+        rb.velocity = new Vector2(vx, vy);
+
+        //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        //이것도 있다
+
+    }
+}
