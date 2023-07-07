9c58d1298f06a0a9eaf150f1d1b38e03697eb453
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-07 11:17:27 +0900

    Create ***.cs

---
+++ 유니티교육/07.07/***.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _07._07
+{
+    internal class Base
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{i}{j} ");
+                }
+                Console.WriteLine(); 
+            }
+            
+
+            int g = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 5; j > g; j--)
+                {
+                    Console.Write("*");
+                }
+                g++;
+                Console.WriteLine();
+            }
+
+            
+
+            int g = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < g; j++)
+                {
+                    Console.Write("*");
+                }
+                g++;
+                Console.WriteLine();
+            }
+
+             실제 풀이
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i + j <= 4)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            */
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i + j >= 4)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
