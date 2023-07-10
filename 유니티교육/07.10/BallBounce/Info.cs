d8e8f1a29c893575b7379a11c69f90126a21b54d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-10 14:08:30 +0900

    Create Info.cs

---
+++ 유니티교육/07.10/BallBounce/Info.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGame
+{
+    public class Info
+    {
+        public int m_nX;
+        public int m_nY;
+    }
+}
