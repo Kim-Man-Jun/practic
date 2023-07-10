0d2e466e2e1c1cd7fc426e5a98be48912ed3f5c7
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-10 14:09:25 +0900

    Create Info2.cs

---
+++ 유니티교육/07.10/Info2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGame
+{
+    public class Info2
+    {
+        public int m_nReady;            //버튼을 누르면 공이 움직임
+        public int m_nX;                //좌표 X
+        public int m_nY;                //좌표 Y
+        public int m_nDirect;           //방향
+        public int m_nSpeed;            //스피드
+    }
+}
