a97f905fa93c7feacc171f9c65e2a3d28f3495d3
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-11 17:42:41 +0900

    Create FieldInfo.cs

---
+++ 유니티교육/07.14/PersonalProject/FieldInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SnakeGame
+{
+    public class FieldInfo
+    {
+        public int FieldX;
+        public int FieldY;
+    }
+}
