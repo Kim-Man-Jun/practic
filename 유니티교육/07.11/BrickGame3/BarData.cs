1fdff4f9aac71a20d1fae56f770cf8a6c510374d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-11 09:45:43 +0900

    Create BarData.cs

---
+++ 유니티교육/07.11/BrickGame3/BarData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BrickGame3
+{
+    public class BarData
+    {
+        public int[] nX = new int[3];       //특문좌표 3개 따로 잡기 ▒▒▒
+        public int nY;                      //Y의 좌표
+    }
+}
