d6590a8c95d970b2338778a0e2d78f402c349f21
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-11 10:58:39 +0900

    Create BarData4.cs

---
+++ 유니티교육/07.11/BrickGame4/BarData4.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace BrickGame4
+{
+    public class BarData4
+    {
+        public int[] nX = new int[3];
+        public int nY;
+    }
+}
