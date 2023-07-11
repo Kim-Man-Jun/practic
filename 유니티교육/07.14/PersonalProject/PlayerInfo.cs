0490bb2693554bc03ea4703f254d96ab62104845
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-11 17:43:03 +0900

    Create PlayerInfo.cs

---
+++ 유니티교육/07.14/PersonalProject/PlayerInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SnakeGame
+{
+    public class PlayerInfo
+    {
+        public int PlayerReady;
+        public int PlayerX;
+        public int PlayerY;
+        public int PlayerSpeed;
+        public int PlayerDirect;
+    }
+}
