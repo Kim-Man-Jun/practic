95d3c4ef3ff50cb1a74dd9d609a0a26639265d9b
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-12 11:04:48 +0900

    Create BlockData.cs

---
+++ 유니티교육/07.12/BrickGame5/BlockData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGame5
+{
+    public class BlockData
+    {
+        public int nLife;       //벽돌을 표시하는 변수
+        public int nX, nY;      //벽돌의 좌표
+
+    }
+}
