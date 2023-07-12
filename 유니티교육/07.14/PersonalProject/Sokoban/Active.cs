641a29b3886d8a5a0e4f8c4b1ac8fbccc649cf80
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-12 17:24:53 +0900

    Create Active.cs

---
+++ 유니티교육/07.14/PersonalProject/Sokoban/Active.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Sokoban
+{
+    public class Active2
+    {
+        static void Main(string[] args)
+        {
+            Sokoban game = new Sokoban();
+            game.Run();
+        }
+    }
+}
