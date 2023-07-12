a6d9f41ae1b0ede9ec64180fe94d846137c8e959
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-12 17:25:04 +0900

    Create Buffer.cs

---
+++ 유니티교육/07.14/PersonalProject/Sokoban/Buffer.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buffer
+{
+    public class ConsoleBuffer
+    {
+        private readonly int fieldx;
+        private readonly int fieldy;
+        private char[,] backBuffer = null;
+        private char[,] frontBuffer = null;
+
+        public ConsoleBuffer(int infieldx, int infieldy)
+        {
+            fieldx = infieldx;
+            fieldy = infieldy;
+            backBuffer = new char[fieldy, fieldx];
+            frontBuffer = new char[fieldy, fieldx];
+            Console.SetWindowSize(fieldx, fieldy + 1);
+            Console.CursorVisible = false;
+        }
+
+        public char[,] Buffer => backBuffer;
+
+        private bool IsRangeOut(int inX, int inY)
+        {
+            if ((inX >= fieldx || inY >= fieldy) || (inX < 0 || inY < 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Draw(char inChar, int inX, int inY)
+        {
+            if (!IsRangeOut(inX, inY))
+            {
+                backBuffer[inY, inX] = inChar;
+            }
+        }
+
+        public void Draw(string inStr, int inX, int inY)
+        {
+            char[] temp = inStr.ToCharArray();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (!IsRangeOut(inX, inY))
+                {
+                    backBuffer[inY, inX + 1] = temp[i];
+                }
+            }
+        }
+
+        private void BufferExtraction()
+        {
+            Array.Copy(backBuffer, frontBuffer, fieldx * fieldy);
+        }
+
+        public void ClearBuffer()
+        {
+            Array.Clear(backBuffer, 0, fieldx * fieldy);
+        }
+
+        private void Print()
+        {
+            for (int y = 0; y < fieldy; y++)
+            {
+                for (int x = 0; x < fieldx; x++)
+                {
+                    Console.Write(frontBuffer[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Show()
+        {
+            BufferExtraction();
+            Console.SetCursorPosition(0, 0);
+            Print();
+        }
+    }
+}
