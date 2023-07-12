37126bad61e4b93d8f44e22d72e84de0d7317e73
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-12 11:04:20 +0900

    Create BallData.cs

---
+++ 유니티교육/07.12/BrickGame5/BallData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGame5
+{
+    public class BallData
+    {
+        public int nReady;
+        public int nX, nY;
+        public int nDirect;
+    }
+}
