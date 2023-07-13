9fb653c8ba0ca8fb9b7f0cec3d50014987b1c861
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-13 11:07:29 +0900

    Create BarData.cs

---
+++ 유니티교육/07.13/BrickFinal/BarData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGameFinal
+{
+    public class BarData
+    {
+        public int[] nX = new int[3];
+        public int nY;
+    }
+}
