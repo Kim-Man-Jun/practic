7bf13adde7f761e8099882d0e876269187cf78a6
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-13 11:07:50 +0900

    Create BlockData.cs

---
+++ 유니티교육/07.13/BrickFinal/BlockData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGameFinal
+{
+    public class BlockData
+    {
+        public int nLife;
+        public int nX, nY;
+    }
+}
