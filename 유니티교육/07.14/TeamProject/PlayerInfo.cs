c07312947b4856221ac4f3bf58c957ee27f87f24
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-14 17:05:31 +0900

    Create PlayerInfo.cs

---
+++ 유니티교육/07.14/TeamProject/PlayerInfo.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace TeamProject
+{
+    public class PlayerInfo
+    {
+        public string PlayerName; //플레이어이름
+        public int PlayerHP; //플레이어 체력 
+        public int CurPlayerHP; // 현재 체력
+        public int PlayerATK; //플레이어공격력
+        public int PotionCT; //포션갯수
+     
+    }
+}
