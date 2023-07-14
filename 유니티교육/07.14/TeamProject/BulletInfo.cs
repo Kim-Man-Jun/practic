7e7e09456216022a0e18d7fe97e00d394e41628e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-14 17:03:09 +0900

    Create BulletInfo.cs

---
+++ 유니티교육/07.14/TeamProject/BulletInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._14_TeamProject
+{
+    //합친부분
+    internal class BulletInfo
+    {
+        public int x;
+        public int y;
+        public bool fire;
+    }
+}
