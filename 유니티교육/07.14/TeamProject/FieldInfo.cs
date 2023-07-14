82c8411044c41f49c082d32a01fcacaa7ec98cd8
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-14 17:03:53 +0900

    Create FieldInfo.cs

---
+++ 유니티교육/07.14/TeamProject/FieldInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._14_TeamProject
+{
+    //합친부분
+    internal class FieldInfo
+    {
+        public int fieldX = 61;     //필드 x 넓이
+        public int fieldY = 60;     //필드 y 높이
+    }
+}
