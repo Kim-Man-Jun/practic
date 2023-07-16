5fece2ddafbd2fe6d5043420e31a9d2066a4f3f1
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-16 19:23:41 +0900

    Update DataManager.cs

--- 2D Platfomer i wanna/DataManager/DataManager.cs
+++ 2D Platfomer i wanna/DataManager/DataManager.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+public class PlayerData
+{
+    public Vector3 PlayerSavePos;
+    public Quaternion PlayerSaveRot;
+}
+
 public class DataManager : MonoBehaviour
 {
-    public class PlayerData
-    {
-        public Vector2 PlayerTransformX;
-        public Vector2 PlayerTransformY;
-    }
+    public PlayerData nowPlayer = new PlayerData();
+    public SaveManager SaveManager = new SaveManager();
 
-    PlayerData nowPlayer = new PlayerData();
     string Path;
     string FileName = "Save";
-    public Transform Player;
 
     //싱글톤 선언
     public static DataManager instance;
+
+    public GameObject Player;
 
     private void Awake()                        //싱글톤 기본
     {
@@ -29,93 +32,41 @@ public class DataManager : MonoBehaviour
         {
             Destroy(instance.gameObject);
         }
-        DontDestroyOnLoad(this.gameObject);
 
         Path = Application.persistentDataPath + "/";
     }
     // Start is called before the first frame update
     void Start()
     {
-        nowPlayer.PlayerTransformX.x = Player.transform.position.x;
-        nowPlayer.PlayerTransformX.y = Player.transform.position.y;
-        Debug.Log(nowPlayer.PlayerTransformX.x);
-        Debug.Log(nowPlayer.PlayerTransformX.y);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
 
     }
 
     public void SaveData()
     {
-        string Data = JsonUtility.ToJson(nowPlayer);
+        nowPlayer.PlayerSavePos = SaveManager.PlayerNowPos;
+        nowPlayer.PlayerSaveRot = SaveManager.PlayerNowRot;
 
+        string Data = JsonUtility.ToJson(nowPlayer);
         File.WriteAllText(Path + FileName, Data);
+        print(Data);
     }
 
     public void LoadData()
     {
-        string Data = File.ReadAllText(Path + FileName);
-        nowPlayer = JsonUtility.FromJson<PlayerData>(Data);
+        string LoadData = File.ReadAllText(Path + FileName);
+        nowPlayer = JsonUtility.FromJson<PlayerData>(LoadData);
+
+        nowPlayer.PlayerSavePos = SaveManager.PlayerNowPos;
+        nowPlayer.PlayerSaveRot = SaveManager.PlayerNowRot;
+
+        Player.transform.position = nowPlayer.PlayerSavePos;
+        Player.transform.rotation = nowPlayer.PlayerSaveRot;
+
+        print(LoadData);
     }
 }
-
-
-/*JSON 저장하는 방법
-1. 저정할 데이터가 존재
-2. 데이터를 JSON으로 변환
-3. JSON을 외부로 저장
-
-JSON을 불러오는 방법
-1. 외부에 저장된 JSON을 가져옴
-2. JSON을 데이터 형태로 변환
-3. 불러온 데이터를 이용
-*/
-
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Save1 : MonoBehaviour
-{
-    [SerializeField]
-    public class SaveTest
-    {
-        public float PlayerX;
-        public float PlayerY;
-    }
-
-    SaveTest test = new SaveTest();
-
-    public GameObject Player;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            test.PlayerX = Player.transform.position.x;
-            test.PlayerY = Player.transform.position.y;
-            Debug.Log(test.PlayerX);
-            Debug.Log(test.PlayerY);
-        }
-    }
-}
-
-
-*/
