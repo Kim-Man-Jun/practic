36298eefd025fcb4da05fec1d8ec72f8181f50b7
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-18 15:44:53 +0900

    Create PlayerController.cs

---
+++ 유니티교육/07.18/1945/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator ani;
+    public float MoveSpeed = 5;
+    public GameObject Bullet = null;
+    public Transform pos = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = MoveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = MoveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        if (Input.GetAxis("Horizontal") >= 0.5f)
+        {
+            ani.SetBool("Right", true);
+        }
+        else
+        {
+            ani.SetBool("Right", false);
+        }
+
+        if (Input.GetAxis("Horizontal") <= -0.5f)
+        {
+            ani.SetBool("Left", true);
+        }
+        else
+        {
+            ani.SetBool("Left", false);
+        }
+
+        if (Input.GetAxis("Vertical") >= 0.5f)
+        {
+            ani.SetBool("Up", true);
+        }
+        else
+        {
+            ani.SetBool("Up", false);
+        }
+
+        transform.Translate(moveX, moveY, 0);
+    }
+}
