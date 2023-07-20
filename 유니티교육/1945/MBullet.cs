69dcbc728712901c745dba1ea8dc9d32e53e081f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-07-20 13:35:07 +0900

    Update MBullet.cs

--- 유니티교육/1945/MBullet.cs
+++ 유니티교육/1945/MBullet.cs
@@ -1,31 +1,25 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using UnityEditor;
 using UnityEngine;
 
 public class MBullet : MonoBehaviour
 {
-    public GameObject target;
-    public float Speed = 3f;
-    Vector2 dir;
-    Vector2 dirNo;
-    public GameObject BoomEffect;
+    public float Speed = 5f;
 
+    public GameObject BoomEffect;
     // Start is called before the first frame update
     void Start()
     {
-        //플레이어 태그로 찾기
-        target = GameObject.FindGameObjectWithTag("Player");
-        //플레이어 - 미사일
-        dir = target.transform.position - transform.position;
-        //방향벡터 구하기, 단위벡터를 1의 크기로 만든다.
-        dirNo = dir.normalized;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(dirNo * Speed * Time.deltaTime);
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
     }
 
     private void OnBecameInvisible()
@@ -37,15 +31,16 @@ public class MBullet : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
             Destroy(gameObject);
+            Destroy(collision.gameObject);
         }
+
     }
 
-    private void OnDestroy()
-    {
-        GameObject go = Instantiate(BoomEffect, transform.position, Quaternion.identity);
-        Destroy(go, 0.6f);
-    }
+    //private void OnDestroy()
+    //{
+    //    GameObject go = Instantiate(BoomEffect, transform.position, Quaternion.identity);
+    //    Destroy(go, 0.6f);
+    //}
 }
 
