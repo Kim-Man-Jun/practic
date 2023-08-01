5a96e748fec153e5784f5efa194d309a7a9978b2
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-01 17:39:22 +0900

    Create Fire.cs

---
+++ 2D Platfomer i wanna/Bullet/Fire.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public Rigidbody2D BulletPrefab;        //프리팹 총알의 rigid 가져옴
+    public Transform firePos;               //플레이어에 붙어있는 총알 발사 지점
+    public float cooltime = 0.25f;          //총알 발사 쿨타임
+    public float curtime = 0;               //총알 발사 후 지나가는 시간
+    public Vector2 PlayerVector;            //플레이어 좌우 구분을 위한 각도 구하기용 벡터값
+    public Vector2 FireposVector;
+    public float BulletSpeed = 30.0f;       //총알 스피드
+
+    private AudioSource AudioSource;
+    public AudioClip Gun;
+
+    private void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");     //플레이어 변수를 만들고 오브젝트 찾기
+        PlayerVector = player.GetComponent<Transform>().position;           //플레이어 위치값 구하기           
+        FireposVector = GetComponent<Transform>().position;                 //현재 오브젝트의 위치값 구하기
+
+        float angle = GetAngle(PlayerVector, FireposVector);                //플레이어와 총구와의 각도 구하기
+
+        if (curtime <= 0)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                if (angle <= -150 && angle >= -170)         //왼쪽일때
+                {
+                    //bullet를 FirePos.transform.position 위치에 FirePos.transform.rotation 회전값으로 복제한다
+                    Rigidbody2D BulletPre = Instantiate(BulletPrefab, firePos.position, Quaternion.identity);
+                    BulletPre.velocity = BulletSpeed * firePos.transform.right * -1;
+                    AudioSource.PlayOneShot(Gun);
+                }
+                else if (angle <= 0 && angle >= -20)        //오른쪽일때
+                {
+                    Rigidbody2D BulletPre = Instantiate(BulletPrefab, firePos.position, Quaternion.identity);
+                    BulletPre.velocity = BulletSpeed * firePos.transform.right;
+                    AudioSource.PlayOneShot(Gun);
+                }
+            }
+            curtime = cooltime;         //총알 발사 후 잠깐의 딜레이 타임
+        }
+        curtime -= Time.deltaTime;
+    }
+
+
+    public static float GetAngle(Vector2 vStart, Vector2 vEnd)
+    {
+        Vector2 v = vEnd - vStart;          //변수 v 생성 
+        return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;       //이거는 알아봐야함
+    }
+}
