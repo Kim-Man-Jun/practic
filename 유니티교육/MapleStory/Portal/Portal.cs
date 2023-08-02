6c6e664a89d0b5cf595df3e3b53d07ac68b5cf1e
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-02 16:21:48 +0900

    Create Portal.cs

---
+++ 유니티교육/MapleStory/Portal/Portal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public string SceneName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
