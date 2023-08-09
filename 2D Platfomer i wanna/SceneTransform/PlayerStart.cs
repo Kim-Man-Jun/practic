04dedc2465a145ca8a8ef0f8c4297c67c91d1087
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-09 17:08:54 +0900

    Update PlayerStart.cs

--- 2D Platfomer i wanna/SceneTransform/PlayerStart.cs
+++ 2D Platfomer i wanna/SceneTransform/PlayerStart.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
+using UnityEngine.SceneManagement;
 
 public class PlayerStart : MonoBehaviour
 {
     PlayerController player;
 
-    // Start is called before the first frame update
-    void Start()
+    static PlayerStart Instance;
+
+    private void Awake()                        //싱글톤 선언과 동시에 플레이어 한개로 고정
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         Vector3 pos = this.gameObject.transform.position;
         player = FindObjectOfType<PlayerController>();
