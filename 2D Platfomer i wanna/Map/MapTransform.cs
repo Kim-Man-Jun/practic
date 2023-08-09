69a0fdbc850ed17b38b0342c47c7326a945caeb0
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-09 17:06:34 +0900

    Update MapTransform.cs

--- 2D Platfomer i wanna/Map/MapTransform.cs
+++ 2D Platfomer i wanna/Map/MapTransform.cs
@@ -1,113 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst;
-using Unity.VisualScripting;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class MapTransform : MonoBehaviour
 {
-    public GameObject AreaCamera;
+    public GameObject Area;
 
     public float LeftX = -1.52f;
     public float DownY = 5.6f;
     public float RightX = 39.4f;
     public float UpY = 29f;
 
-    public int RoomNum = 0;
-    private int nowRoomNum;
+    public int RoomNum;
 
     public bool MapMoving = false;
-    public float MovingSpeed = 50f;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        nowRoomNum = RoomNum;
+        ChangeRoom();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        //switch (nowRoomNum)
-        //{
-        //    case 0:
-        //        AreaCamera.transform.position = new Vector2(LeftX, DownY);
-        //        break;
-        //    case 1:
-        //        AreaCamera.transform.position = new Vector2(RightX, DownY);
-        //        break;
-        //    case 2:
-        //        AreaCamera.transform.position = new Vector2(RightX, UpY);
-        //        break;
-        //    case 3:
-        //        AreaCamera.transform.position = new Vector2(LeftX, UpY);
-        //        break;
-        //}
 
-        if (MapMoving == true)
+    }
+
+    public void ChangeRoom()
+    {
+        PlayerController player = FindObjectOfType<PlayerController>();
+
+        switch (player.PlayerNowRoom)
         {
-            if (nowRoomNum == 0)
-            {
-                AreaCamera.transform.position = new Vector2(LeftX, DownY);
+            case 0:
+                Area.transform.position = new Vector3(LeftX, DownY, 0);
+                break;
 
-                if (AreaCamera.transform.position.x == LeftX
-                    && AreaCamera.transform.position.y == DownY)
-                {
-                    MapMoving = false;
-                }
-            }
+            case 1:
+                Area.transform.position = new Vector3(RightX, DownY, 0);
+                break;
 
-            else if (nowRoomNum == 1)
-            {
-                AreaCamera.transform.position = new Vector2(RightX, DownY);
+            case 2:
+                Area.transform.position = new Vector3(RightX, UpY, 0);
+                break;
 
-                if (AreaCamera.transform.position.x == RightX
-                    && AreaCamera.transform.position.y == DownY)
-                {
-                    MapMoving = false;
-                }
-            }
+            case 3:
+                Area.transform.position = new Vector3(LeftX, UpY, 0);
+                break;
 
-            else if (nowRoomNum == 2)
-            {
-                AreaCamera.transform.position = new Vector2(RightX, UpY);
-            }
-
-            else if (nowRoomNum == 3)
-            {
-                AreaCamera.transform.position = new Vector2(LeftX, UpY);
-            }
-
-            else
-            {
-                return;
-            }
+            default:
+                break;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print(nowRoomNum);
-        print(MapMoving);
+        PlayerController player = FindObjectOfType<PlayerController>();
+
         if (collision.gameObject.tag == "Player")
         {
-            if (MapMoving == false)
+            if (player.PlayerNowRoom < RoomNum)
             {
-                if (nowRoomNum <= RoomNum)
-                {
-                    nowRoomNum++;
-                    MapMoving = true;
-                }
-
-                if (nowRoomNum >= RoomNum)
-                {
-                    nowRoomNum--;
-                    MapMoving = true;
-                }
+                player.PlayerNowRoom++;
+                ChangeRoom();
             }
 
-            //if (nowRoomNum == RoomNum)
-            //{
-            //    return;
-            //}
+            else if (player.PlayerNowRoom > RoomNum)
+            {
+                player.PlayerNowRoom--;
+                ChangeRoom();
+            }
+
+            else if (player.PlayerNowRoom == RoomNum)
+            {
+            }
         }
     }
 }
