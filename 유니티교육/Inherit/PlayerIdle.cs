ac9e87adada3a3f7ec0d34b6a1f9ff4d828afaaf
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-10 11:15:28 +0900

    Create PlayerIdle.cs

---
+++ 유니티교육/Inherit/PlayerIdle.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerState
+{
+    public PlayerIdleState(Player _player, PlayerStateMachine _stateMachine,
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            player.stateMachine.ChangeState(player.moveState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+}
