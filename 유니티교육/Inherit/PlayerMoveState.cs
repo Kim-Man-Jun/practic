0f346709843e3b1a84365585a8bc1ae1c9f91717
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-10 11:15:42 +0900

    Create PlayerMoveState.cs

---
+++ 유니티교육/Inherit/PlayerMoveState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : PlayerState
+{
+    public PlayerMoveState(Player _player, PlayerStateMachine _stateMachine, 
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            player.stateMachine.ChangeState(player.idleState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+}
