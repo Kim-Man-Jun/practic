e96bcae6c2e49bf58d67282cd4bcc94296138932
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-10 11:15:18 +0900

    Create Player.cs

---
+++ 유니티교육/Inherit/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator anim { get; private set; }
+    public PlayerStateMachine stateMachine {  get; private set; }
+
+    public PlayerIdleState idleState { get; private set; }
+
+    public PlayerMoveState moveState { get; private set; }
+
+    private void Awake()
+    {
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
+    }
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+} 
