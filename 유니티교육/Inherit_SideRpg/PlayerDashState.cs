b569cff0ddcbc5847620eb6827eb8169b9eb9468
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-11 17:35:30 +0900

    Create PlayerDashState.cs

---
+++ 유니티교육/Inherit_SideRpg/PlayerDashState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashState : PlayerState
+{
+    public PlayerDashState(Player _player, PlayerStateMachine _stateMachine, 
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        stateTimer = player.dashDuration;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        player.SetVelocity(0, rb.velocity.y);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(!player.isGroundDetected() && player.isWallDetected())
+        {
+            stateMachine.ChangeState(player.wallSlideState);
+        }
+
+        //rb.velocity.y에 0을 넣을 경우 중력값에 영향을 받지 않음
+        player.SetVelocity(player.DashSpeed * player.facingDir, rb.velocity.y);
+
+        if(stateTimer < 0)
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+    }
+}
