68be17ff4a38296ab908f82c1326a8995bb3160d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-11 17:33:50 +0900

    Update Player.cs

--- 유니티교육/Inherit_SideRpg/Player.cs
+++ 유니티교육/Inherit_SideRpg/Player.cs
@@ -4,12 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Attack Details")]
+    public Vector2[] attackMovement;
+
+    public bool isBusy { get; private set; }
+
+    [Header("Move Info")]
+    public float moveSpeed = 12f;
+    public float jumpForce;
+
+    [Header("Dash Info")]
+    [SerializeField] private float dashCooldown = 1.5f;
+    private float dashUsageTimer;
+    public float DashSpeed;
+    public float dashDuration;
+    public float dashDir { get; private set; }
+
+    [Header("Collision Info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
     public Animator anim { get; private set; }
-    public PlayerStateMachine stateMachine {  get; private set; }
+    public Rigidbody2D rb { get; private set; }
+
+    public SpriteRenderer sr { get; private set; }
+
+    public PlayerStateMachine stateMachine { get; private set; }
 
     public PlayerIdleState idleState { get; private set; }
 
     public PlayerMoveState moveState { get; private set; }
+
+    public PlayerJumpState jumpState { get; private set; }
+
+    public PlayerAirState airState { get; private set; }
+
+    public PlayerDashState dashState { get; private set; }
+
+    public PlayerWallSlideState wallSlideState { get; private set; }
+
+    public PlayerWallJumpState wallJumpState { get; private set; }
+
+    public PlayerPrimaryAttackState primaryAttack { get; private set; }
 
     private void Awake()
     {
@@ -17,16 +59,106 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash");
+        wallSlideState = new PlayerWallSlideState(this, stateMachine, "WallSlide");
+        wallJumpState = new PlayerWallJumpState(this, stateMachine, "Jump");
+        primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
     }
 
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
         stateMachine.Initialize(idleState);
     }
 
     private void Update()
     {
         stateMachine.currentState.Update();
+
+        CheckForDashInput();
     }
-} 
+
+    public IEnumerator BusyFor(float _seconds)
+    {
+        isBusy = true;
+       
+        yield return new WaitForSeconds(_seconds);
+      
+        isBusy = false;
+    }
+    
+    public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
+
+    //어디서든 대시를 사용할 수 있음 
+    private void CheckForDashInput()
+    {
+        if(isWallDetected())
+        {
+            return;
+        }
+
+        dashUsageTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        {
+            dashUsageTimer = dashCooldown;
+
+            dashDir = Input.GetAxisRaw("Horizontal");
+
+            if(dashDir == 0)
+            {
+                dashDir = facingDir;
+            }
+            stateMachine.ChangeState(dashState);
+        }
+    }
+
+    public void ZeroVelocity()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+                FlipController(_xVelocity);
+    }
+
+    public bool isGroundDetected() => Physics2D.Raycast(groundCheck.position,
+        Vector2.down, groundCheckDistance, whatIsGround);
+
+    public bool isWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right
+        * facingDir, wallCheckDistance, whatIsGround);
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x,
+            groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x
+            + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if(_x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(_x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+}
