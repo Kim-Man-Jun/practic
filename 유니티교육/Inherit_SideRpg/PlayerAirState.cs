81e8a6f6c27ca5b4d61af7f0e0cc6f97ee88c2f0
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-11 17:34:06 +0900

    Create PlayerAirState.cs

---
+++ 유니티교육/Inherit_SideRpg/PlayerAirState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerAirState : PlayerState
+{
+    public PlayerAirState(Player _player, PlayerStateMachine _stateMachine,
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (player.isGroundDetected())
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+
+        if (xInput != 0)
+        {
+            player.SetVelocity(player.moveSpeed * 0.8f * xInput, rb.velocity.y);
+        }
+
+        if(player.isWallDetected())
+        {
+            stateMachine.ChangeState(player.wallSlideState);
+        }
+    }
+}
