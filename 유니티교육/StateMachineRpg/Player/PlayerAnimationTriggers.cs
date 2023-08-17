78ccd616c524921a3f5934cbe028ae7baf8de40d
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:28:14 +0900

    Create PlayerAnimationTriggers.cs

---
+++ 유니티교육/StateMachineRpg/Player/PlayerAnimationTriggers.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationTriggers : MonoBehaviour
+{
+    private Player player => GetComponentInParent<Player>();
+
+    private void AnimationTrigger()
+    {
+        player.AnimationTrigger();
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.attackCheck.position,
+            player.attackCheckRadius);
+
+        foreach(var hit in colliders)
+        {
+            if(hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+}
