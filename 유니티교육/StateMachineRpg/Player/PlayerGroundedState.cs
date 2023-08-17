920ed79a833a11244c223750bbbb9720234ed4f6
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:28:46 +0900

    Create PlayerGroundedState.cs

---
+++ 유니티교육/StateMachineRpg/Player/PlayerGroundedState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundedState : PlayerState
+{
+    public PlayerGroundedState(Player _player, PlayerStateMachine _stateMachine,
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            stateMachine.ChangeState(player.primaryAttack);
+        }
+
+        if(!player.isGroundDetected())
+        {
+            stateMachine.ChangeState(player.airState);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && player.isGroundDetected())
+        {
+            stateMachine.ChangeState(player.jumpState);
+        }
+    }
+}
