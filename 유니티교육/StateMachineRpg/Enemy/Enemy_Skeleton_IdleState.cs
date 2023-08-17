5f12f368f11c37d3c1f6627aa154f64eae9f810b
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:25:57 +0900

    Create Enemy_Skeleton_IdleState.cs

---
+++ 유니티교육/StateMachineRpg/Enemy/Enemy_Skeleton_IdleState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Skeleton_IdleState : Enemy_Skeleton_GroundedState
+{
+    public Enemy_Skeleton_IdleState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName,
+        Enemy_Skeleton _enemy) : base(_enemyBase, _stateMachine, _animBoolName, _enemy)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        enemy.Flip();
+
+        stateTimer = enemy.idleTime;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (stateTimer < 0)
+        {
+            stateMachine.ChangeState(enemy.moveState);
+        }
+    }
+}
