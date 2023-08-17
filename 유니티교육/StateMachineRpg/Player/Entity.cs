3032bded1c22d0896bfe60bd37676dbc9ab3bede
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:26:47 +0900

    Create Entity.cs

---
+++ 유니티교육/StateMachineRpg/Player/Entity.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Entity : MonoBehaviour
+{
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public SpriteRenderer sr { get; private set; }
+
+    [Header("Collision Info")]
+
+    public Transform attackCheck;
+    public float attackCheckRadius;
+
+    [SerializeField] protected Transform groundCheck;
+    [SerializeField] protected float groundCheckDistance;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected LayerMask whatIsGround;
+
+    public int facingDir { get; private set; } = 1;
+    protected bool facingRight = true;
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    public virtual void Damage()
+    {
+        Debug.Log(gameObject.name + " 데미지");
+    }
+
+    public virtual bool isGroundDetected() => Physics2D.Raycast(groundCheck.position,
+        Vector2.down, groundCheckDistance, whatIsGround);
+
+    public virtual bool isWallDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right
+        * facingDir, wallCheckDistance, whatIsGround);
+
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x,
+            groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x
+            + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawWireSphere(attackCheck.position, attackCheckRadius);
+    }
+
+    public virtual void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public virtual void FlipController(float _x)
+    {
+        if (_x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (_x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    public void ZeroVelocity()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
+    }
+
+}
