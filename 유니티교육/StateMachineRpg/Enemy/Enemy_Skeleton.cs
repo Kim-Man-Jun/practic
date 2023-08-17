e90f8318baf8e83e749339a831f601bf8bac350a
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:24:44 +0900

    Create Enemy_Skeleton.cs

---
+++ 유니티교육/StateMachineRpg/Enemy/Enemy_Skeleton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Enemy_Skeleton : Enemy
+{
+    public Enemy_Skeleton_IdleState idleState { get; private set; }
+    public Enemy_Skeleton_MoveState moveState { get; private set; }
+    public Enemy_Skeleton_BattleState battleState { get; private set; }
+    public Enemy_Skeleton_AttackState attackState { get; private set; }
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        idleState = new Enemy_Skeleton_IdleState(this, stateMachine, "Idle", this);
+        moveState = new Enemy_Skeleton_MoveState(this, stateMachine, "Move", this);
+        battleState = new Enemy_Skeleton_BattleState(this, stateMachine, "Move", this);
+        attackState = new Enemy_Skeleton_AttackState(this, stateMachine, "Attack", this);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        stateMachine.Initialize(idleState);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
