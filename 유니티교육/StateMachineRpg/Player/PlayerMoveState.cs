e879bd348f4aee25040b1494775c485e48ca0e4f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:30:07 +0900

    Create PlayerMoveState.cs

---
+++ 유니티교육/StateMachineRpg/Player/PlayerMoveState.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : PlayerGroundedState
+{
+    public PlayerMoveState(Player _player, PlayerStateMachine _stateMachine,
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (xInput == 0 || player.isWallDetected())
+        {
+            player.stateMachine.ChangeState(player.idleState);
+        }
+
+        //else if (xInput > 0)
+        //{
+        //    player.sr.flipX = false;
+        //}
+
+        //else if (xInput < 0)
+        //{
+        //    player.sr.flipX = true;
+        //}
+        
+        player.SetVelocity(xInput * player.moveSpeed, rb.velocity.y);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+}
