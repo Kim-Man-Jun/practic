cb21e401ed44fab02855ff00240c6cdd37671d97
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:25:01 +0900

    Create Enemy_Skeleton_AnimationTriggers.cs

---
+++ 유니티교육/StateMachineRpg/Enemy/Enemy_Skeleton_AnimationTriggers.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Skeleton_AnimationTriggers : MonoBehaviour
+{
+    private Enemy_Skeleton enemy => GetComponentInParent<Enemy_Skeleton>();
+
+    private void AnimationTrigger()
+    {
+        enemy.AnimationFinishTrigger();
+    }
+
+    void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(enemy.attackCheck.position,
+            enemy.attackCheckRadius);
+
+        foreach(var hit in colliders)
+        {
+            if(hit.GetComponent<Player>() != null)
+            {
+                hit.GetComponent<Player>().Damage();
+            }
+        }
+    }
+}
