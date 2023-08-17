c976a63bf35c7af5f2b2a3b73337d42d4d80517f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:30:50 +0900

    Create PlayerState.cs

---
+++ 유니티교육/StateMachineRpg/Player/PlayerState.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    protected Rigidbody2D rb;
+
+    protected float xInput;
+    protected float yInput;
+    private string animBoolName;
+
+    protected float stateTimer;
+
+    protected bool triggerCalled;
+
+#region component
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, 
+    string _animBoolName)
+    {
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
+    }
+#endregion
+
+
+public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+        rb = player.rb;
+        triggerCalled = false;
+    }
+
+    public virtual void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
+
+        stateTimer -= Time.deltaTime;
+    }
+
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
+    }
+
+    public virtual void AnimationFinishTrigger()
+    {
+        triggerCalled = true;
+    }
+}
