00636f54ca4a163037ff71a9d3883448dcf9e6b4
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-17 15:27:45 +0900

    Create Player.cs

---
+++ 유니티교육/StateMachineRpg/Player/Player.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : Entity
+{
+    [Header("Attack Details")]
+    public Vector2[] attackMovement;
+
+    public bool isBusy { get; private set; }
+
+    [Header("Move Info")]
+    public float moveSpeed = 12f;
+    public float jumpForce;
+
+    [Header("Dash Info")]
+    [SerializeField] private float dashCooldown = 1.5f;
+    private float dashUsageTimer;
+    public float DashSpeed;
+    public float dashDuration;
+    public float dashDir { get; private set; }
+
+    public PlayerStateMachine stateMachine { get; private set; }
+
+    public PlayerIdleState idleState { get; private set; }
+
+    public PlayerMoveState moveState { get; private set; }
+
+    public PlayerJumpState jumpState { get; private set; }
+
+    public PlayerAirState airState { get; private set; }
+
+    public PlayerDashState dashState { get; private set; }
+
+    public PlayerWallSlideState wallSlideState { get; private set; }
+
+    public PlayerWallJumpState wallJumpState { get; private set; }
+
+    public PlayerPrimaryAttackState primaryAttack { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState = new PlayerAirState(this, stateMachine, "Jump");
+        dashState = new PlayerDashState(this, stateMachine, "Dash");
+        wallSlideState = new PlayerWallSlideState(this, stateMachine, "WallSlide");
+        wallJumpState = new PlayerWallJumpState(this, stateMachine, "Jump");
+        primaryAttack = new PlayerPrimaryAttackState(this, stateMachine, "Attack");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        stateMachine.Initialize(idleState);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        stateMachine.currentState.Update();
+
+        CheckForDashInput();
+    }
+
+    public IEnumerator BusyFor(float _seconds)
+    {
+        isBusy = true;
+       
+        yield return new WaitForSeconds(_seconds);
+      
+        isBusy = false;
+    }
+    
+    public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
+
+    //어디서든 대시를 사용할 수 있음 
+    private void CheckForDashInput()
+    {
+        if(isWallDetected())
+        {
+            return;
+        }
+
+        dashUsageTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashUsageTimer < 0)
+        {
+            dashUsageTimer = dashCooldown;
+
+            dashDir = Input.GetAxisRaw("Horizontal");
+
+            if(dashDir == 0)
+            {
+                dashDir = facingDir;
+            }
+            stateMachine.ChangeState(dashState);
+        }
+    }
+}
