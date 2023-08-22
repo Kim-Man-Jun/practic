4aa6af098b9c1cf40e7adf5b0ceb17ca9807a091
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-22 17:41:06 +0900

    Update Entity.cs

--- 유니티교육/StateMachineRpg/Player/Entity.cs
+++ 유니티교육/StateMachineRpg/Player/Entity.cs
@@ -5,9 +5,17 @@ using UnityEngine.UIElements;
 
 public class Entity : MonoBehaviour
 {
+    #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
     public SpriteRenderer sr { get; private set; }
+    public EntityFX fx { get; private set; }
+    #endregion
+
+    [Header("Knockback Info")]
+    [SerializeField] protected Vector2 knockbackDirection;
+    [SerializeField] protected float knockbackDuration;
+    protected bool isKnocked;
 
     [Header("Collision Info")]
 
@@ -33,6 +41,7 @@ public class Entity : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponentInChildren<SpriteRenderer>();
+        fx = GetComponentInChildren<EntityFX>();
     }
 
     protected virtual void Update()
@@ -42,7 +51,17 @@ public class Entity : MonoBehaviour
 
     public virtual void Damage()
     {
+        fx.StartCoroutine("FlashFX");
+        StartCoroutine(HitKnockback());
         Debug.Log(gameObject.name + " 데미지");
+    }
+
+    protected virtual IEnumerator HitKnockback()
+    {
+        isKnocked = true;
+        rb.velocity = new Vector2(knockbackDirection.x * -facingDir, knockbackDirection.y);
+        yield return new WaitForSeconds(knockbackDuration);
+        isKnocked = false;
     }
 
     public virtual bool isGroundDetected() => Physics2D.Raycast(groundCheck.position,
@@ -81,11 +100,21 @@ public class Entity : MonoBehaviour
     }
     public void ZeroVelocity()
     {
+        if(isKnocked)
+        {
+            return;
+        }
+
         rb.velocity = Vector2.zero;
     }
 
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
+        if(isKnocked)
+        {
+            return;
+        }
+
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
         FlipController(_xVelocity);
     }
