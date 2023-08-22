c7bb9cd743157bcee075477317315fa7486115b4
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-22 17:38:34 +0900

    Update Enemy.cs

--- 유니티교육/StateMachineRpg/Enemy/Enemy.cs
+++ 유니티교육/StateMachineRpg/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEditor.Timeline;
 using UnityEngine;
 
@@ -15,6 +16,13 @@ public class Enemy : Entity
     public float attackCooldown;
     [HideInInspector] public float lasttimeAttacked;
 
+
+    [Header("Stunned Info")]
+    public float stunDuration;
+    public Vector2 stunDirection;
+    protected bool canBeStunned;
+    [SerializeField] protected GameObject counterImage;
+
     public EnemyStateMachine stateMachine { get; private set; }
 
     [SerializeField] protected LayerMask whatIsPlayer;
@@ -29,6 +37,29 @@ public class Enemy : Entity
     {
         base.Update();
         stateMachine.currentState.Update();
+    }
+
+    public virtual void OpenCounterAttackWindow()
+    {
+        canBeStunned = true;
+        counterImage.SetActive(true);
+    }
+
+    public virtual void CloseCounterAttackWindow()
+    {
+        canBeStunned = false;
+        counterImage.SetActive(false);
+    }
+
+    public virtual bool CanBeStunned()
+    {
+        if(canBeStunned)
+        {
+            CloseCounterAttackWindow();
+            return true;
+        }
+
+        return false;
     }
 
     public virtual void AnimationFinishTrigger() 
