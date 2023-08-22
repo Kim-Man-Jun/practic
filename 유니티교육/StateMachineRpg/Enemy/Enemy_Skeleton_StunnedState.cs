f3dc32a61aeb908df9ad227b992215a918184392
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-22 17:40:49 +0900

    Create Enemy_Skeleton_StunnedState.cs

---
+++ 유니티교육/StateMachineRpg/Enemy/Enemy_Skeleton_StunnedState.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Skeleton_StunnedState : EnemyState
+{
+    private Enemy_Skeleton enemy;
+
+    public Enemy_Skeleton_StunnedState(Enemy _enemyBase, EnemyStateMachine _stateMachine,
+        string _animBoolName, Enemy_Skeleton _enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        enemy.fx.InvokeRepeating("RedColorBlink", 0, 0.1f);
+
+        stateTimer = enemy.stunDuration;
+
+        rb.velocity = new Vector2(-enemy.facingDir * enemy.stunDirection.x, enemy.stunDirection.y);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        enemy.fx.Invoke("CancelRedBlink", 0);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer < 0)
+        {
+            stateMachine.ChangeState(enemy.idleState);
+        }
+    }
+}
