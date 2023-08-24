d5b2ae50d7514aec60ef90c92597f7983ec9600a
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-24 15:49:48 +0900

    Create PlayerAimSwordState.cs

---
+++ 유니티교육/StateMachineRpg/Player/PlayerAimSwordState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimSwordState : PlayerState
+{
+    public PlayerAimSwordState(Player _player, PlayerStateMachine _stateMachine,
+        string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player.skill.sword.DotsActive(true);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
+    }
+}
