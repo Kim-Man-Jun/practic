42c258f51dd45e5145c02b41af6085e787eff3d4
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-25 12:28:45 +0900

    Update PlayerCatchSwordState.cs

--- 유니티교육/StateMachineRpg/Player/PlayerCatchSwordState.cs
+++ 유니티교육/StateMachineRpg/Player/PlayerCatchSwordState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerCatchSwordState : PlayerState
 {
+    Transform sword;
+
     public PlayerCatchSwordState(Player _player, PlayerStateMachine _stateMachine,
         string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
@@ -13,15 +15,33 @@ public class PlayerCatchSwordState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        sword = player.sword.transform;
+
+        if (player.transform.position.x > sword.position.x && player.facingDir == 1)
+        {
+            player.Flip();
+        }
+        else if (player.transform.position.x < sword.position.x && player.facingDir == -1)
+        {
+            player.Flip();
+        }
+
+        rb.velocity = new Vector2(player.swordReturnImpact * -player.facingDir, rb.velocity.y);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        player.StartCoroutine("BusyFor", 0.2f);
     }
 
     public override void Update()
     {
         base.Update();
+        if(triggerCalled)
+        {
+            stateMachine.ChangeState(player.idleState);
+        }
     }
 }
