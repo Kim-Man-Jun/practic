9cadf15d31b007cef2cebc7481048565dc96bf77
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-25 12:28:30 +0900

    Update PlayerAimSwordState.cs

--- 유니티교육/StateMachineRpg/Player/PlayerAimSwordState.cs
+++ 유니티교육/StateMachineRpg/Player/PlayerAimSwordState.cs
@@ -19,15 +19,29 @@ public class PlayerAimSwordState : PlayerState
     public override void Exit()
     {
         base.Exit();
+        player.StartCoroutine("BusyFor", 0.2f);
     }
 
     public override void Update()
     {
         base.Update();
 
+        player.ZeroVelocity();
+
         if(Input.GetKeyUp(KeyCode.Mouse1))
         {
             stateMachine.ChangeState(player.idleState);
+        }
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(player.transform.position.x > mousePosition.x && player.facingDir == 1)
+        {
+            player.Flip();
+        }
+        else if(player.transform.position.x < mousePosition.x && player.facingDir == -1)
+        {
+            player.Flip();
         }
     }
 }
