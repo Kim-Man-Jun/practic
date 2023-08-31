5c4b98a6135a0f525d60cb7c1e9752474e11bf7f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-08-31 10:29:32 +0900

    Update PlayerStart.cs

--- 2D Platfomer i wanna/SceneTransform/PlayerStart.cs
+++ 2D Platfomer i wanna/SceneTransform/PlayerStart.cs
@@ -11,24 +11,17 @@ public class PlayerStart : MonoBehaviour
 
     static PlayerStart Instance;
 
-    private void Awake()                        //싱글톤 선언과 동시에 플레이어 한개로 고정
+    private void Awake()                        
     {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
+        //싱글톤 선언과 동시에 플레이어 한개로 고정
+        //if (Instance != null)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+        //Instance = this;
+        //DontDestroyOnLoad(gameObject);
 
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
         Vector3 pos = this.gameObject.transform.position;
         player = FindObjectOfType<PlayerController>();
 
@@ -41,10 +34,52 @@ public class PlayerStart : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+
+    }
+
+    //private void OnEnable()
+    //{
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+    //}
+
+    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+    //    Vector3 pos = this.gameObject.transform.position;
+    //    player = FindObjectOfType<PlayerController>();
+
+    //    if (player != null)
+    //    {
+    //        if (DataManager.StageFirst == false)
+    //        {
+    //            player.transform.position = pos;
+    //        }
+    //    }
+    //}
+
     // Update is called once per frame
     void Update()
     {
 
     }
+
+    //public void PlayerStartPos(int stagenum)
+    //{
+    //    if(stagenum == 1)
+    //    {
+    //        transform.position = new Vector3(-29.3f, -6.47f, 0);
+    //        return;
+    //    }
+    //    else if (stagenum == 2)
+    //    {
+    //        transform.position = new Vector3(-18.17f, -6.32f, 0);
+    //        return;
+    //    }
+    //    else if (stagenum == 3)
+    //    {
+    //        return;
+    //    }
+    //}
 }
 
