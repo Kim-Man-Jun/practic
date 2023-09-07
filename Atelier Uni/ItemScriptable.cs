075dee51236f3e49be3f3f629d27a16706d058fd
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-09-07 17:51:24 +0900

    Create ItemScriptable.cs

---
+++ Atelier Uni/ItemScriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_consumables", menuName = "Item/Item_consumables")]
+public class ItemScriptable : ItemCategory_consumables
+{
+    public int ItemNumber;
+}
