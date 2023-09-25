b57ee23eaae1e1d685eb4344aa1096b93abe1a09
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-09-25 16:12:17 +0900

    Create ParkourAction.cs

---
+++ 유니티교육/ParkourGame/ParkourActions/ParkourAction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Parkour Menu/Create New Parkour Action")]
+public class ParkourAction : ScriptableObject
+{
+    [Header("Checking Obstacle height")]
+    [SerializeField] string animationName;
+    [SerializeField] float minimumHeight;
+    [SerializeField] float maximumHeight;
+
+    [Header("Rotating Player towards Obstacle")]
+    [SerializeField] bool lookAtObstacle;
+
+    public Quaternion RequiredRotation { get; set; }
+
+    [Header("Target Matching")]
+    [SerializeField] bool allowtargetMatching = true;
+    [SerializeField] AvatarTarget compareBodyPart;
+    [SerializeField] float compareStartTime;
+    [SerializeField] float compareEndTime;
+
+    public Vector3 ComparePosition { get; set; }
+
+    public bool CheckIfAvailable(ObstacleInfo hitData, Transform player)
+    {
+        float checkHeight = hitData.heightInfo.point.y - player.position.y;
+
+        if (checkHeight < minimumHeight || checkHeight > maximumHeight)
+        {
+            return false;
+        }
+
+        if (lookAtObstacle)
+        {
+            RequiredRotation = Quaternion.LookRotation(-hitData.hitInfo.normal);
+        }
+
+        if (allowtargetMatching)
+        {
+            ComparePosition = hitData.heightInfo.point;
+
+
+        }
+
+        return true;
+    }
+
+    public string AnimationName => animationName;
+    public bool LookAtObstacle => lookAtObstacle;
+    public bool AllowTargetMatching => allowtargetMatching;
+    public AvatarTarget CompareBodyPart => compareBodyPart;
+    public float CompareStartTime => compareStartTime;
+    public float CompareEndTime => compareEndTime;
+}
