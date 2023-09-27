240b85c291d1dff322904c85a4ae6b35dcd1afcd
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-09-27 16:15:34 +0900

    Update ParkourAction.cs

--- 유니티교육/ParkourGame/Parkour/ParkourAction.cs
+++ 유니티교육/ParkourGame/Parkour/ParkourAction.cs
@@ -7,11 +7,13 @@ public class ParkourAction : ScriptableObject
 {
     [Header("Checking Obstacle height")]
     [SerializeField] string animationName;
+    [SerializeField] string barrierTag;
     [SerializeField] float minimumHeight;
     [SerializeField] float maximumHeight;
 
     [Header("Rotating Player towards Obstacle")]
     [SerializeField] bool lookAtObstacle;
+    [SerializeField] float parkourActionDelay;
 
     public Quaternion RequiredRotation { get; set; }
 
@@ -20,11 +22,18 @@ public class ParkourAction : ScriptableObject
     [SerializeField] AvatarTarget compareBodyPart;
     [SerializeField] float compareStartTime;
     [SerializeField] float compareEndTime;
+    [SerializeField] Vector3 comparePositionWeight = new Vector3(0, 1, 0);
 
     public Vector3 ComparePosition { get; set; }
 
     public bool CheckIfAvailable(ObstacleInfo hitData, Transform player)
     {
+        if (!string.IsNullOrEmpty(barrierTag)
+            && hitData.hitInfo.transform.tag != barrierTag)
+        {
+            return false;
+        }
+
         float checkHeight = hitData.heightInfo.point.y - player.position.y;
 
         if (checkHeight < minimumHeight || checkHeight > maximumHeight)
@@ -40,8 +49,6 @@ public class ParkourAction : ScriptableObject
         if (allowtargetMatching)
         {
             ComparePosition = hitData.heightInfo.point;
-
-
         }
 
         return true;
@@ -49,8 +56,10 @@ public class ParkourAction : ScriptableObject
 
     public string AnimationName => animationName;
     public bool LookAtObstacle => lookAtObstacle;
+    public float ParkourActionDelay => parkourActionDelay;
     public bool AllowTargetMatching => allowtargetMatching;
     public AvatarTarget CompareBodyPart => compareBodyPart;
     public float CompareStartTime => compareStartTime;
     public float CompareEndTime => compareEndTime;
+    public Vector3 ComparePositionWeight => comparePositionWeight;
 }
