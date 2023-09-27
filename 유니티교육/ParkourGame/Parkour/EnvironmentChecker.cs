69c64f39789882ee664966aeefad4334303ff87f
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-09-27 16:15:47 +0900

    Update EnvironmentChecker.cs

--- 유니티교육/ParkourGame/Parkour/EnvironmentChecker.cs
+++ 유니티교육/ParkourGame/Parkour/EnvironmentChecker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnvironmentChecker : MonoBehaviour
 {
@@ -8,6 +9,15 @@ public class EnvironmentChecker : MonoBehaviour
     public float raylength = 0.9f;
     public float heightRayLength = 6f;
     public LayerMask obstacleLayer;
+
+    [Header("Check Ledge")]
+    [SerializeField] float ledgeRayLength = 11f;
+    [SerializeField] float ledgeRayHeightThreshold = 0.76f;
+
+    [Header("Climbing Check")]
+    [SerializeField] float climbingRayLength = 1.6f;
+    [SerializeField] LayerMask climbingLayer;
+    public int numberOfRays = 12;
 
     public ObstacleInfo CheckObstacle()
     {
@@ -33,6 +43,70 @@ public class EnvironmentChecker : MonoBehaviour
 
         return hitData;
     }
+
+    public bool CheckLedge(Vector3 movementDirection, out LedgeInfo ledgeInfo)
+    {
+        ledgeInfo = new LedgeInfo();
+
+        if (movementDirection == Vector3.zero)
+            return false;
+
+        float ledgeOriginOffset = 0.5f;
+        var ledgeOrigin = transform.position + movementDirection
+            * ledgeOriginOffset + Vector3.up;
+
+        if (Physics.Raycast(ledgeOrigin, Vector3.down, out RaycastHit hit,
+            ledgeRayLength, obstacleLayer))
+        {
+            Debug.DrawRay(ledgeOrigin, Vector3.down * ledgeRayLength, Color.blue);
+
+
+            var surfaceRaycastorigin = transform.position + movementDirection
+                - new Vector3(0, 0.1f, 0);
+
+            if (Physics.Raycast(surfaceRaycastorigin, -movementDirection,
+                out RaycastHit surfaceHit, 2, obstacleLayer))
+            {
+                float Ledgeheight = transform.position.y - hit.point.y;
+
+                if (Ledgeheight > ledgeRayHeightThreshold)
+                {
+                    ledgeInfo.angle = Vector3.Angle(transform.forward, surfaceHit.normal);
+                    ledgeInfo.height = Ledgeheight;
+                    ledgeInfo.surfacehit = surfaceHit;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckClimbing(Vector3 climbDirection, out RaycastHit climbInfo)
+    {
+        climbInfo = new RaycastHit();
+
+        if (climbDirection == Vector3.zero)
+        {
+            return false;
+        }
+
+        var climbOrigin = transform.position + Vector3.up * 1.5f;
+        var climbOffset = new Vector3(0, 0.19f, 0);
+
+        for (int i = 0; i < numberOfRays; i++)
+        {
+            Debug.DrawRay(climbOrigin + climbOffset * i, climbDirection, Color.red);
+
+            if (Physics.Raycast(climbOrigin + climbOffset * i, climbDirection,
+                out RaycastHit hit, climbingRayLength, climbingLayer))
+            {
+                climbInfo = hit;
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 public struct ObstacleInfo
@@ -41,4 +115,11 @@ public struct ObstacleInfo
     public bool heightHitFound;
     public RaycastHit hitInfo;
     public RaycastHit heightInfo;
+}
+
+public struct LedgeInfo
+{
+    public float angle;
+    public float height;
+    public RaycastHit surfacehit;
 }
